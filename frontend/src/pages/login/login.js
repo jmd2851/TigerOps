dd8900aaa0c3e91778eeba19b181ce0f2bc3a291
thea a <ta7384@g.rit.edu>
2023-10-02 13:08:53 -0400

    applied changes

--- frontend/src/pages/login/login.js
+++ frontend/src/pages/login/login.js
@@ -8,7 +8,7 @@ export default function Login() {
         <div className="loginPage">
             <img src={logo} className="App-logo" alt="St. Peter's Kitchen logo" />
             <div className="loginContainer">
-                <div className="login">
+                <form action="" method="POST" onsubmit="" className="login">
                     <div className="header">
                         <p className="headerTitle">login</p>
                         <p className="bodyText">enter your email address and password to log in.</p>
@@ -16,22 +16,22 @@ export default function Login() {
 
                     <div className='inputs'>
                         <TextField fullWidth id="standard-basic" label="Email" variant="filled" margin="normal" />
-                        <TextField fullWidth id="standard-basic" label="Password" variant="filled" margin="normal" />
+                        <TextField type="password" fullWidth id="standard-basic" label="Password" variant="filled" margin="normal" />
                     </div>
 
                     <div className='forgotPasswordContainer'>
-                        <p className='subtitle'>Forgot password?</p>
+                        <p className='subtitle'><a href="">Forgot password?</a></p>
                     </div>
 
                     <div className='loginButtonContainer'>
-                        <Button variant="contained">submit</Button>
+                        <Button id="loginButton" variant="contained">submit</Button>
                     </div>
-                </div>
+                </form>
 
                 <div className="visitor">
                     <div className="header">
                         <p className="headerTitle">no account?</p>
-                        <p className="bodyText">click here to view St. Peter's Kitchen's menu, news, reminders and events.</p>
+                        <p className="bodyText">click the button underneath to view St. Peter's Kitchen's menu, news, reminders and events.</p>
                     </div>
                     
                     <div className='visitorButtonContainer'>
