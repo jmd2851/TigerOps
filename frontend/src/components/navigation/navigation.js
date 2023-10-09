16af4cfdf532a431a50699b150fcd8db7e8f9701
conor goodwin <cpg3179@g.rit.edu>
2023-10-08 20:42:55 -0400

    updated styles, added new components path, and js

---
+++ frontend/src/components/navigation/navigation.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './navigation.css';
+
+export class Navigation extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <div className='navContainer'>
+            <div className = "nav">
+                        <h1 className = "User">
+                            Admin
+                        </h1>
+                        <div className='calendarOptions'>
+                            <div className='topOptions'>
+                                <h2 className='links'>
+                                    Events
+                                </h2>
+                            </div>
+                            <div className='topOptions'>
+                                <h2 className = "links">
+                                    Calendar View
+                                </h2>
+                            </div>
+                        </div>
+                        <div className='border'>
+                           
+                        </div>
+                            <div className='bottomOptions'>
+                                <h2 className='links'>
+                                    Configuration
+                                </h2>
+                            </div>
+                                <h2 className='links'>
+                                    Help
+                                </h2>
+                            <div className='bottomOptions'>
+                                <h2 className='links'>
+                                    Log Out
+                                </h2>
+                            </div>
+            </div>
+            </div>
+          );
+    }
+};
