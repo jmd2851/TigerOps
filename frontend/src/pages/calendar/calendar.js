357df133d081639f4dfa40b177318d840dd9c471
Will Lu <will@wl6181.student.rit.edu>
2023-10-11 14:00:58 -0400

    fix compile bug and add route to calendar page

--- frontend/src/pages/calendar/calendar.js
+++ frontend/src/pages/calendar/calendar.js
@@ -1,33 +1,25 @@
-import ReactDOM from 'react-dom/client';
-import './styles.css';
-import * as React from 'react';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
-import { Navigation } from '../../components/navigation/navigation.js'
-
+import "./styles.css";
+import * as React from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { Navigation } from "../../components/navigation";
 
 export default function Calendar() {
   return (
-    <div className='calendarContainer'>
+    <div className="calendarContainer">
       <Navigation />
-      <div className='calendar'>
-        <h1>
-          Calendar View
-        </h1>
+      <div className="calendar">
+        <h1>Calendar View</h1>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateCalendar/>
+          <DateCalendar />
         </LocalizationProvider>
-        <div className='event'> </div>
-        <p className='description'>
-            Tap the <div className='circle'></div> on the calendar to check out the schedule for that day.
+        <div className="event"> </div>
+        <p className="description">
+          Tap the <div className="circle"></div> on the calendar to check out
+          the schedule for that day.
         </p>
       </div>
     </div>
   );
 }
-
-
-const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(<Calendar />);
