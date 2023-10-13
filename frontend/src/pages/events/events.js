af820eb11e948eedbb100372b1ce21b21a4f59f8
Conorg56 <conorg309@gmail.com>
2023-10-13 14:25:59 -0400

    updated the event page to include the nav and updated the styles

--- frontend/src/pages/events/events.js
+++ frontend/src/pages/events/events.js
@@ -2,12 +2,17 @@ import './styles.css';
 import Page from '../../components/Page';
 import PageHeader from '../../components/PageHeader';
 import Slideshow from '../../components/Slideshow';
+import Navigation from '../../components/navigation.js';
 
 export default function Events() {
     return (
+        <div className='eventContainer'>
+            <Navigation/>
         <Page>
+            
             <PageHeader title="Weekly Events" />
             <Slideshow />
         </Page>
+        </div>
     )
 }
