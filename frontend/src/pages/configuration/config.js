f7b67067f8b275ff4ed39542863d31d5f98fa417
Conorg56 <conorg309@gmail.com>
2023-10-13 14:31:44 -0400

    intial commit for config page

---
+++ frontend/src/pages/configuration/config.js
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export default function Configuration() {
+    return (
+       <div>
+            <h1>
+                Config Page
+            </h1>
+       </div>
+    )
+}
