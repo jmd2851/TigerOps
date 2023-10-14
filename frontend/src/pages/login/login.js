ffb9ec4e7542a8591c192d591becbc5c9e474ab1
Will Lu <will@Wills-MacBook-Pro-8.local>
2023-10-14 17:35:45 -0400

    Handle login request

--- frontend/src/pages/login/login.js
+++ frontend/src/pages/login/login.js
@@ -1,46 +1,107 @@
-import logo from '../../assets/images/logo.png';
-import './styles.css';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import logo from "../../assets/images/logo.png";
+import "./styles.css";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
 
-export default function Login() {
-    return (
-        <div className="loginPage">
-            <img src={logo} className="App-logo" alt="St. Peter's Kitchen logo" />
-            <div className="loginContainer">
-                <form action="" method="POST" onsubmit="" className="login">
-                    <div className="header">
-                        <p className="headerTitle">login</p>
-                        <p className="bodyText">enter your email address and password to log in.</p>
-                    </div>
+export default function Login(props) {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
 
-                    <div className='inputs'>
-                        <TextField fullWidth id="standard-basic" label="Email" variant="filled" margin="normal" />
-                        <TextField type="password" fullWidth id="standard-basic" label="Password" variant="filled" margin="normal" />
-                    </div>
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setState((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
 
-                    <div className='forgotPasswordContainer'>
-                        <p className='subtitle'><a href="">Forgot password?</a></p>
-                    </div>
+  const handleLogin = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:4000/login", {
+      method: "POST",
+      body: JSON.stringify({
+        email: state.email,
+        password: state.password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      credentials: "include",
+    })
+      .then((response) => {
+        if (!response.ok) throw new Error(response.status);
+        else return response.json();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-                    <div className='loginButtonContainer'>
-                        <Button id="loginButton" variant="contained">submit</Button>
-                    </div>
-                </form>
+  return (
+    <div className="loginPage">
+      <img src={logo} className="App-logo" alt="St. Peter's Kitchen logo" />
+      <div className="loginContainer">
+        <div className="login">
+          <div className="header">
+            <p className="headerTitle">login</p>
+            <p className="bodyText">
+              enter your email address and password to log in.
+            </p>
+          </div>
 
-                <div className="visitor">
-                    <div className="header">
-                        <p className="headerTitle">no account?</p>
-                        <p className="bodyText">click the button below to view St. Peter's Kitchen's menu, news, reminders and events.</p>
-                    </div>
-                    
-                    <div className='visitorButtonContainer'>
-                        <Button fullWidth variant="contained" >continue as viewer</Button>
-                    </div>
-                </div>
-            </div>
+          <div className="inputs">
+            <TextField
+              fullWidth
+              label="Email"
+              id="email"
+              variant="filled"
+              margin="normal"
+              onChange={handleChange}
+            />
+            <TextField
+              type="password"
+              fullWidth
+              id="password"
+              label="Password"
+              variant="filled"
+              margin="normal"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="forgotPasswordContainer">
+            <p className="subtitle">
+              <a href="">Forgot password?</a>
+            </p>
+          </div>
+
+          <div className="loginButtonContainer">
+            <Button id="loginButton" variant="contained" onClick={handleLogin}>
+              submit
+            </Button>
+          </div>
         </div>
-    )
-}
 
-//function to use login API
+        <div className="visitor">
+          <div className="header">
+            <p className="headerTitle">no account?</p>
+            <p className="bodyText">
+              click the button below to view St. Peter's Kitchen's menu, news,
+              reminders and events.
+            </p>
+          </div>
+
+          <div className="visitorButtonContainer">
+            <Button fullWidth variant="contained">
+              continue as viewer
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
