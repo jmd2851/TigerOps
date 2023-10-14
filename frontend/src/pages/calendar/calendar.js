547d2e55562129f1cc39143e83017576b8bff2a5
thea a <ta7384@g.rit.edu>
2023-10-14 14:55:03 -0400

    calendar page account for nav bar

--- frontend/src/pages/calendar/calendar.js
+++ frontend/src/pages/calendar/calendar.js
@@ -3,22 +3,26 @@ import * as React from "react";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import PageHeader from "../../components/PageHeader";
+import Page from "../../components/Page";
 
 
 export default function Calendar() {
   return (
-    <div className="calendarContainer">
-      <div className="calendar">
-        <h1>Calendar View</h1>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateCalendar />
-        </LocalizationProvider>
-        <div className="event"> </div>
-        <p className="description">
-          Tap the <div className="circle"></div> on the calendar to check out
-          the schedule for that day.
-        </p>
-      </div>
+    <Page>
+      <PageHeader title="Calendar View" />
+      <div className="calendarContainer">
+        <div className="calendar">
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateCalendar />
+          </LocalizationProvider>
+          <div className="event"> </div>
+          <p className="description">
+            Tap the <div className="circle"></div> on the calendar to check out
+            the schedule for that day.
+          </p>
+        </div>
     </div>
+    </Page>
   );
 }
