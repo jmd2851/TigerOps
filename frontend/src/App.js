5fb31698a9564c9305090796b731552acece4074
Conorg56 <conorg309@gmail.com>
2023-10-16 18:28:15 -0400

    updated the routing to include help and config page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,17 @@ import { Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login/login";
 import Events from "./pages/events/events";
 import Calendar from "./pages/calendar/calendar";
+import Help from "./pages/help/help.js";
+import Configuration from "./pages/configuration/configuration.js";
 
 const App = () => {
   return (
     <Routes>
       <Route exact path="/login" element={<Login />} />
       <Route exact path="/events" element={<Events />} />
-      <Route exact path="/calendar" element={<Calendar />} />
+      <Route exact path="/Calendar" element={<Calendar />} />
+      <Route exact path="/Help" element={<Help />} />
+      <Route exact path="/Configuration" element={<Configuration />} />
     </Routes>
   );
 };
