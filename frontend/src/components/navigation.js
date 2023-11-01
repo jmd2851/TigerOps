dd123e44dd2c973902fbbb7f6f886f3e0b0abbab
thea a <ta7384@g.rit.edu>
2023-11-01 02:47:51 -0400

    added logout modal

--- frontend/src/components/navigation.js
+++ frontend/src/components/navigation.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import "./navigation.css";
 import { Link } from "react-router-dom";
 import AppContext from "../AppContext";
 import axios from "axios";
+import { Box, Button, Modal, Stack, Typography } from "@mui/material";
+
+const boxStyle = {
+  position: 'absolute',
+  top: '24%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+}
 
 export default function Navigation() {
+  const [open,setOpen] = useState(false);
+  const handleOpen = () => {
+      setOpen(true);
+  }
+  const handleClose = () => {
+      setOpen(false);
+  }
+
   const { user, setUser } = React.useContext(AppContext);
-
   const role = user ? user.UserRole : "Guest";
-
   const handleLogout = () => {
     axios
       .post("http://localhost:4000/logout", {}, { withCredentials: true })
@@ -22,6 +43,19 @@ export default function Navigation() {
 
   return (
     <div className="navContainer">
+      <Modal open={open} onClose={handleClose}>
+          <Box sx={{...boxStyle, maxWidth:'180px', borderRadius:'10px', padding: '24px'}}>
+              <Typography variant="h4" sx={{textAlign:'center',padding:'16px 0', marginBottom: '16px'}}>Log out?</Typography>
+
+              <Stack direction="row" spacing={2}>
+                  <Button variant="contained" sx={{width:'50%'}}>
+                      <Link to="/login">Yes</Link>
+                  </Button>
+                  <Button variant="contained" sx={{width:'50%'}} onClick={handleClose}>No</Button>
+              </Stack>
+          </Box>
+      </Modal>
+
       <div className="nav">
         <h1 className="User">{role.toUpperCase()}</h1>
         <div className="calendarOptions">
@@ -49,9 +83,10 @@ export default function Navigation() {
         {user != null && (
           <div className="bottomOptions">
             <h2 className="links">
-              <Link to="/events" onClick={handleLogout}>
-                Log Out
-              </Link>
+              <Box onClick={() => {
+                handleOpen();
+                handleLogout();
+              }}><Link>Log Out</Link></Box>
             </h2>
           </div>
         )}
