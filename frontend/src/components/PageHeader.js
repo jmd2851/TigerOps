03f263281fa2c0bcbd5e78715fdabd3ee76cd467
thea a <ta7384@g.rit.edu>
2023-11-01 19:02:47 -0400

    information page

--- frontend/src/components/PageHeader.js
+++ frontend/src/components/PageHeader.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './PageHeader.css';
 import { Typography } from '@mui/material';
 
-export default function PageHeader({title,subtitle}) {
+export default function PageHeader({title,subtitle,sx}) {
     return (
-        <div className='heading'>
+        <div className='heading' styles={{...sx}}>
             <Typography variant="h4">{title}</Typography>
             <Typography variant="body1">{subtitle}</Typography>
         </div>
