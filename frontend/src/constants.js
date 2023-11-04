0c50b352932641737157272ef4348b27f2cc45be
Will Lu <will@Wills-MacBook-Pro-8.local>
2023-11-03 22:45:34 -0400

    link edit page to backend

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -7,3 +7,15 @@ export const FormTypes = {
   CREATE: 0,
   EDIT: 1,
 };
+
+export class Slide {
+  constructor(title, subheader, body, type, data, start, end = null) {
+    this.title = title;
+    this.subheader = subheader;
+    this.body = body;
+    this.type = type;
+    this.data = data;
+    this.start = start;
+    this.end = end;
+  }
+}
