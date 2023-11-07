b3fa3578d010502941eec2e62147869149143948
thea a <ta7384@g.rit.edu>
2023-11-06 22:12:37 -0500

    styling

--- frontend/src/pages/create/create.js
+++ frontend/src/pages/create/create.js
@@ -1,11 +1,15 @@
 import "./styles.css";
 import Page from "../../components/Page";
-import PageHeader from "../../components/PageHeader";
-import { Button } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 import { useState } from "react";
 import EventForm from "./EventForm";
 import MenuForm from "./MenuForm";
 import { FormTypes, SlideTypes } from "../../constants";
+
+const buttonStyle = {
+  width: '20%',
+  padding: '16px 8px'
+}
 
 export default function Create() {
   const [type, setType] = useState(null);
@@ -15,25 +19,41 @@ export default function Create() {
       title="Create a Slide"
       subtitle="Choose the Slide type and fill out its details"
     >
-      <Button
-        variant="contained"
-        onClick={() => {
-          setType(SlideTypes.EVENT);
-        }}
-      >
-        event
-      </Button>
+      <Stack direction="column" spacing={6}>
+        <Stack direction="column" spacing={2}>
+          <Typography variant="h6">
+            What type of slide would you like to create?
+          </Typography>
 
-      <Button
-        variant="contained"
-        onClick={() => {
-          setType(SlideTypes.MENU);
-        }}
-      >
-        menu
-      </Button>
-      {type === SlideTypes.EVENT && <EventForm formType={FormTypes.CREATE} />}
-      {type === SlideTypes.MENU && <MenuForm formType={FormTypes.CREATE} />}
+          <Stack direction="row" spacing={2}>
+            <Button
+              variant="contained"
+              onClick={() => {
+                setType(SlideTypes.EVENT);
+              }}
+              sx={{...buttonStyle}}
+            >
+              event
+            </Button>
+
+            <Button
+              variant="contained"
+              onClick={() => {
+                setType(SlideTypes.MENU);
+              }}
+              sx={{...buttonStyle}}
+            >
+            menu
+            </Button>
+          </Stack>
+        </Stack>
+        
+        <Box sx={{width:'60%', paddingLeft:'2px'}}>
+          {type === SlideTypes.EVENT && <EventForm formType={FormTypes.CREATE} />}
+          {type === SlideTypes.MENU && <MenuForm formType={FormTypes.CREATE} />}
+        </Box>
+
+      </Stack>
     </Page>
   );
 }
